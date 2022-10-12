f0a0f7d00e84b0b2cdcd7ee6f236689b0f083406
psc-utb <psc.utb@gmail.com>
2022-10-12 17:05:17 +0200

    Area Admin a zobrazení pro správu Carousels + Vytvoření oblasti (area) Admin + přidání konfigurace routování pro areas + vytvoření CarouselController v area Admin + vytvoření view Select.cshtml s tabulkou CarouselItem (pro správu adminem) + zkopírování _ViewStart a _ViewImports do area Admin + přidání odkazu v navbaru na Select pro Carousel v Admin

---
+++ UTB.Eshop.Web/Areas/Admin/Controllers/CarouselController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UTB.Eshop.Web.Models.Database;
+using UTB.Eshop.Web.Models.Entities;
+
+namespace UTB.Eshop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarouselController : Controller
+    {
+        public IActionResult Select()
+        {
+            List<CarouselItem> carouselItems = DatabaseFake.CarouselItems;
+            return View(carouselItems);
+        }
+    }
+}
--- UTB.Eshop.Web/Startup.cs
+++ UTB.Eshop.Web/Startup.cs
@@ -49,6 +49,10 @@ namespace UTB.Eshop.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
