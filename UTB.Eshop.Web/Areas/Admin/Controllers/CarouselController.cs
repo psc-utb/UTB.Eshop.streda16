ebbcc5cdd1cff1afe2b1101519f69a807102c340
psc-utb <psc.utb@gmail.com>
2022-10-19 17:36:24 +0200

    Create, Edit, Delete pro Carousel

--- UTB.Eshop.Web/Areas/Admin/Controllers/CarouselController.cs
+++ UTB.Eshop.Web/Areas/Admin/Controllers/CarouselController.cs
@@ -16,5 +16,65 @@ namespace UTB.Eshop.Web.Areas.Admin.Controllers
             List<CarouselItem> carouselItems = DatabaseFake.CarouselItems;
             return View(carouselItems);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarouselItem carouselItem)
+        {
+            if (DatabaseFake.CarouselItems.Count > 0)
+            {
+                carouselItem.ID = DatabaseFake.CarouselItems.Last().ID + 1;
+            }
+            else
+                carouselItem.ID = 1;
+
+            DatabaseFake.CarouselItems.Add(carouselItem);
+            return RedirectToAction(nameof(Select));
+        }
+
+        public IActionResult Edit(int ID)
+        {
+            CarouselItem carouselItem = DatabaseFake.CarouselItems.FirstOrDefault(carItem => carItem.ID == ID);
+
+            if (carouselItem != null)
+            {
+                return View(carouselItem);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CarouselItem carouselItemFromForm)
+        {
+            CarouselItem carouselItem = DatabaseFake.CarouselItems.FirstOrDefault(carItem => carItem.ID == carouselItemFromForm.ID);
+
+            if (carouselItem != null)
+            {
+                carouselItem.ImageSrc = carouselItemFromForm.ImageSrc;
+                carouselItem.ImageAlt = carouselItemFromForm.ImageAlt;
+
+                return RedirectToAction(nameof(Select));
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Delete(int ID)
+        {
+            CarouselItem carouselItem = DatabaseFake.CarouselItems.FirstOrDefault(carItem => carItem.ID == ID);
+        
+            if (carouselItem != null)
+            {
+                DatabaseFake.CarouselItems.Remove(carouselItem);
+                return RedirectToAction(nameof(Select));
+            }
+
+            return NotFound();
+        }
     }
 }
